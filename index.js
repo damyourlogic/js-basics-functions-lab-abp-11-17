ed6874a15f51592e423129d415c5d29c0dd7f832
Amy Jackson <aj81623@gmail.com>
2017-11-07 23:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(){
-
-  
+function distanceFromHqInBlocks(pickup){
+return Math.abs(pickup - 42);
 }
 
 function distanceFromHqInFeet(){
   distanceFromHqInBlocks();
-  
+
 }
